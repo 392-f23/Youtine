c0e2362b5d500c62113ce01488fc8ec49f9a26b7
LitpangC <mark.li.438@outlook.com>
2023-10-27 16:56:31 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+};
+
+export default App;
