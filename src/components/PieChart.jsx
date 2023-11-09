98d59724395c5f00f3d43a1a959ebe52aa0687b7
cmorcott <coelmorcott2024@u.northwestern.edu>
2023-11-09 15:53:58 -0600

    Working on PieChart

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import { getDatabase, ref, onValue } from "firebase/database";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const PieChart = () => {
-  const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
-    datasets: [
-      {
-        data: [12, 19, 3, 5, 2],
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#2ECC71', '#9B59B6'],
-        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#2ECC71', '#9B59B6'],
-      },
-    ],
-  };
+const PieChart = ({ selectedDate }) => {
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: [],
+      hoverBackgroundColor: [],
+    }],
+  });
+
+  useEffect(() => {
+    const db = getDatabase();
+    const tasksRef = ref(db, 'tasks');
+    onValue(tasksRef, (snapshot) => {
+      const tasksData = snapshot.val();
+      const newLabels = [];
+      const newData = [];
+      const backgroundColors = ['#FF6384', '#36A2EB', '#FFCE56', '#2ECC71', '#9B59B6']; // Add more colors if needed
+
+      let totalPercentage = 0;
+      tasksData.forEach((task, index) => {
+        if (task.progress[selectedDate]) {
+          const percentDone = (task.progress[selectedDate] / task.daily_goal) * 100;
+          totalPercentage += percentDone;
+
+          newLabels.push(task.title);
+          newData.push(percentDone);
+        }
+      });
+
+      // Normalize the data if totalPercentage exceeds 100%
+      const normalizedData = totalPercentage > 100 ? newData.map(data => (data / totalPercentage) * 100) : newData;
+
+      setChartData({
+        labels: newLabels,
+        datasets: [{
+          data: normalizedData,
+          backgroundColor: backgroundColors.slice(0, newLabels.length), // Only use as many colors as there are labels
+          hoverBackgroundColor: backgroundColors.slice(0, newLabels.length),
+        }],
+      });
+    });
+  }, [selectedDate]);
 
   return (
     <div>
-      <h2>Your Progress</h2>
-      <Pie data={data} />
+      <h2>A Balanced Life is Best, Here is Where Your Progress Is</h2>
+      <Pie data={chartData} />
     </div>
   );
 };
--- src/components/SummaryPage.jsx
+++ src/components/SummaryPage.jsx
@@ -4,16 +4,22 @@ import 'react-calendar/dist/Calendar.css';
 import "./SummaryPage.css";
 import PieChart from './PieChart';
 import OneDaySummary from "./OneDaySummary";
+
+
 const SummaryPage = ({routines}) => {
     
     const [value, onChange] = useState(new Date());
+
+    const formatDate = (date) => {
+        return date.toISOString().split('T')[0];
+    };
 
     return (
         <div className= "calendar-page">
             <h2 className="main-title">Summary</h2>
             <Calendar className="calendar" onChange={onChange} value={value} />
             <OneDaySummary routines={routines} date = {value}/>
-            { < PieChart /> }
+            { < PieChart selectedDate={formatDate(value)} /> }
         </div>
     );
     
